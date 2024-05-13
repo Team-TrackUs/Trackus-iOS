4a09a5a3271bf7f6e5c39490d28947ecdede42bf
Seokki-Kwon <seokki0706@gmail.com>
2024-05-13 16:06:10 +0900

    [Feat] 위치권한 설정

---
+++ TrackUs-iOS/TrackUs-iOS/Common/Shared/LocationService.swift
@@ -0,0 +1,35 @@
+//
+//  LocationService.swift
+//  TrackUs-iOS
+//
+//  Created by 석기권 on 5/13/24.
+//
+
+import Foundation
+import CoreLocation
+import UIKit
+
+final class LocationService: NSObject, CLLocationManagerDelegate {
+    static let shared = LocationService()
+    let locationManager = CLLocationManager()
+    var currentLocation : CLLocationCoordinate2D?
+    var userLocationDelegate: UserLocationDelegate?
+    
+    private override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // 가장 높은수준의 정확도
+        locationManager.distanceFilter = 10 // 특정거리를 이동할때마다 업데이트 받도록 filter
+        locationManager.startUpdatingLocation()
+        currentLocation = locationManager.location?.coordinate
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        currentLocation = manager.location?.coordinate
+        
+        if userLocationDelegate != nil {
+            userLocationDelegate?.userLocationUpated(location: locations.first!)
+        }
+    }
+}
--- TrackUs-iOS/TrackUs-iOS/Presentation/Running/Controllers/RunningHomeVC.swift
+++ TrackUs-iOS/TrackUs-iOS/Presentation/Running/Controllers/RunningHomeVC.swift
@@ -6,24 +6,41 @@
 //
 
 import UIKit
+import MapKit
 
-class RunningHomeVC: ViewController {
-
+final class RunningHomeVC: ViewController {
+    private var mapView: MKMapView!
+    private let locationService = LocationService.shared
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .yellow
+        checkLocationStatus()
+        setupMapView()
+        setMapRegion()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // 맵설정
+    func setupMapView() {
+        mapView = MKMapView(frame: self.view.bounds)
+        mapView.showsUserLocation = true
+        self.view.addSubview(mapView)
     }
-    */
-
+    
+    // 초기위치 설정
+    func setMapRegion() {
+        let defaultSpanValue = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let defaultLocation = CLLocationCoordinate2D(latitude: 37.57050030731104, longitude:  126.97888892151437)
+        
+        if let currentLocation = locationService.currentLocation {
+            mapView.setRegion(.init(center: currentLocation, span: defaultSpanValue), animated: true)
+        } else {
+            mapView.setRegion(.init(center: defaultLocation, span: defaultSpanValue), animated: true)
+        }
+    }
+    
+    // 위치권한 확인
+    func checkLocationStatus() {
+        if LocationService.shared.locationManager.authorizationStatus != .authorizedWhenInUse {
+            LocationService.shared.locationManager.requestWhenInUseAuthorization()
+        }
+    }
 }
