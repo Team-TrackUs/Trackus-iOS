978c70bbb87cafc6f7373ddbb82ac792b15e6f20
github Choe-ju <github juwon3415@gmail.com>
2024-06-20 21:11:08 +0900

    [Fix] 사용자 기본 프로필 레이아웃 수정

--- TrackUs-iOS/TrackUs-iOS/Common/Shared/ImageCacheManager.swift
+++ TrackUs-iOS/TrackUs-iOS/Common/Shared/ImageCacheManager.swift
@@ -259,13 +259,17 @@ extension UIImageView {
     
     /// 프로필 이미지 불러오기 -> url: String? - 없을 경우 기본 이미지 반환
     func loadProfileImage(url: String?, ompletionHandler: @escaping () -> Void) {
+        self.layer.borderColor = UIColor.gray3.cgColor
         if let url = url{
             ImageCacheManager.shared.memoryloadImage(imageUrl: url) { image in
                 self.image = image
+                self.layer.borderWidth = 1
                 ompletionHandler()
             }
         }else {
             self.image = UIImage(systemName: "person.crop.circle.fill")?.withTintColor(.gray3)
+            self.tintColor = .gray3
+            self.layer.borderWidth = 3
             ompletionHandler()
         }
     }
